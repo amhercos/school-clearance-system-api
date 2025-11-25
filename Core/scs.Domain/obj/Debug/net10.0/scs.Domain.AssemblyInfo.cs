87a8e9d5115b17aef51af2d8506792568affac17
amher <amhercosainetimpahan@gmail.com>
2025-11-26 04:09:24 +0800

    Modified RootNameSpace

--- Core/scs.Domain/obj/Debug/net10.0/scs.Domain.AssemblyInfo.cs
+++ Core/scs.Domain/obj/Debug/net10.0/scs.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Scs.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaa90fdafd4690888af24c5666d9a256ab747a3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Scs.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Scs.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
