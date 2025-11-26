5aa442ff280a3e42b8c5a26ec38f41facf72df1a
amher <amhercosainetimpahan@gmail.com>
2025-11-27 04:27:04 +0800

    new assembly namespaces and user dtos and controllers

---
+++ Core/scs.Application/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Scs.Application.DTOs;
+
+namespace Scs.Application.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDto>> GetAllUsersAsync();
+        Task<UserDto?> GetUserByIdAsync(int userId);
+    }
+}
---
+++ Core/scs.Application/Services/UserService.cs
@@ -0,0 +1,36 @@
+﻿using Scs.Application.DTOs;
+using Scs.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scs.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IEnumerable<UserDto>> GetAllUsersAsync()
+        {
+            var users = await _userRepository.GetAllAsync();
+
+            // Map Domain Entities to DTOs using LINQ
+            return users.Select(u => new UserDto(u.UserId, u.FirstName, u.LastName, u.Email));
+        }
+
+        public async Task<UserDto?> GetUserByIdAsync(int userId)
+        {
+            var user = await _userRepository.GetByIdAsync(userId);
+
+            // Map if user is found, otherwise return null
+            return user != null
+                ? new UserDto(user.UserId, user.FirstName, user.LastName, user.Email)
+                : null;
+        }
+    }
+}
